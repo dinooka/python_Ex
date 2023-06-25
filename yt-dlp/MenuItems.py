7e37474870cb966befae220c9f6e60b86ecad440
dinooka <dinooka7@gmail.com>
2023-06-25 19:05:00 +0530

    yt-dlp downloader completed

--- yt-dlp/MenuItems.py
+++ yt-dlp/MenuItems.py
@@ -2,7 +2,7 @@ import operations as op
 
 def dwnld_video_options():
     url = input("Enter the url: ")
-    print("Enter the download location(Type 'd' for default location, else give the folder path...)")
+    print("\nEnter the download location(Press 'Enter' for default location, else give the folder path...)")
     location = input("Download location: ")
     print("\na. Select the quality of the video")
     print("b. Download the best audio/video quality video")
@@ -14,8 +14,34 @@ def dwnld_video_options():
             op.download_video(quality,url)
         else:            
             op.download_video(quality,url,location)       
-    else:
-        op.download_best_video(url) 
+    elif((download_option == 'b')):
+        if(location == ''):
+            op.download_best_video(url)
+        else:            
+            op.download_best_video(url,location)
+
+def dwnld_playlist_options():
+    url = input("Enter the url: ")
+    print("\nEnter the download location(Press 'Enter' for default location, else give the folder path...)")
+    location = input("Download location: ")
+    resolution = input("Enter the video resolution: ")
+    print("a. Download an entire playlist")
+    print("b. Download a partial playlist")
+    download_option = input("Select the above options: ")
+    
+    if(download_option=='a'):
+        if(location == ''):                        
+            op.download_playlist(url,1,resolution)
+        else:                      
+            op.download_playlist(url,1,resolution,location)
+    elif((download_option == 'b')):
+        start_index = input("Enter the start index: ")
+        end_index = input("Enter the end index: ")
+        if(location == ''):
+            op.download_partial_playlist(url,resolution,start_index,end_index)
+        else:            
+            op.download_partial_playlist(url,resolution,start_index,end_index,location)
+        
 
 def main_menu():
         
@@ -31,5 +57,4 @@ def main_menu():
     print("4. Update the Application")
     print("5. Close the Application")
 
-    menuoption = input("\nSelect an option from above list(Type the number): ")
-    return menuoption
+    return input("\nSelect an option from above list(Type the number): ")
--- yt-dlp/operations.py
+++ yt-dlp/operations.py
@@ -17,7 +17,7 @@ def version_check():
     command = "yt-dlp --version"
     execute_cmd(command,"Version check")
 
-def download_video(height:int, url:str, folder_path="F:\FireDM Downloads"):
+def download_video(height:str, url:str, folder_path="F:\FireDM Downloads"):
     command = [
         "yt-dlp",
         "-f", f"bestvideo[height<={height}][ext=mp4]+bestaudio",
@@ -27,6 +27,54 @@ def download_video(height:int, url:str, folder_path="F:\FireDM Downloads"):
 
     execute_cmd(command,"Download")
 
-def download_best_video(url:str):
-    command = f"yt-dlp {url}"
-    execute_cmd(command,"download best video")
+def download_best_video(url:str,folder_path ="F:\FireDM Downloads"):
+    
+    command = [
+        "yt-dlp",
+        "-f", f"bestvideo[ext=mp4]+bestaudio",
+        "-o", f"{folder_path}\%(title)s.mp4",
+        url
+    ]
+    execute_cmd(command,"Downloading best video")
+
+def download_partial_playlist(url:str,height:str,start_index=1,end_index=None,folder_path ="F:\FireDM Downloads"):
+
+    command = [
+        'yt-dlp',
+        url,
+        '--playlist-start',
+        str(start_index),
+        '--playlist-end',
+        str(end_index),
+        '-f',
+        f'bestvideo[height<={height}][ext=mp4]+bestaudio',
+        '--merge-output-format',
+        'mp4',
+        '-o',
+        f'{folder_path}/%(title)s.%(ext)s',
+        '--continue'
+    ]
+    execute_cmd(command,"Downloading playlist")
+
+def download_playlist(url, start_index, h, folder_path="F:\FireDM Downloads", end_index=None):
+    command = [
+        'yt-dlp',
+        url,
+        '--playlist-start',
+        str(start_index),
+    ]
+    
+    if end_index is not None:
+        command.extend(['--playlist-end', str(end_index)])
+        
+    command.extend([
+        '-f',
+        f'bestvideo[height<={h}][ext=mp4]+bestaudio',
+        '--merge-output-format',
+        'mp4',
+        '-o',
+        f'{folder_path}/%(title)s.%(ext)s',
+        '--continue'
+    ])
+
+    execute_cmd(command,"Downloading entire playlist")
